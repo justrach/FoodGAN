39dd9381f4be1ca2661460c9b5da87ba48e7603c
Rach <54503978+justrach@users.noreply.github.com>
2021-05-24 16:03:29 +0800

    add

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from fastai import *
 from fastai.vision import *
+import fastai
 # import fastai
 import PIL 
 import torchvision.transforms as T
@@ -8,14 +9,14 @@ from torch.utils.data import DataLoader, Dataset
 # import tensorflow as tf
 # from PIL import Image
 # inferencer = load_learner(path)
+
+
+
 def load_model(path=".", model_name="model.pkl"):
     learn = load_learner(path, fname=model_name)
     return learn 
+
 model = load_model('models')
-
-
-
-
 
 
 def predict(img, n: int = 3) -> Dict[str, Union[str, List]]:
@@ -29,22 +30,15 @@ def predict(img, n: int = 3) -> Dict[str, Union[str, List]]:
         predictions.append(
             {"class": image_class.replace("_", " "), "output": output, "prob": prob}
         )
-
     predictions = sorted(predictions, key=lambda x: x["output"], reverse=True)
     predictions = predictions[0:n]
     return {"class": str(pred_class), "predictions": predictions}
-
-
-
-
 def load_image(img_path):
     img = tf.io.read_file(img_path)
     img = tf.io.decode_jpeg(img, channels = 3)
     img = tf.image.resize(img, size = (512, 512), method = 'bicubic')
     img = img / 255.0
     return img
-
-
 img_bytes = st.file_uploader("Squash It!!", type=['png', 'jpg', 'jpeg'])
 if img_bytes is not None:
     # image = PIL.create(file_uploaded)
@@ -53,7 +47,8 @@ if img_bytes is not None:
     img_pil = PIL.Image.open(img_bytes)
     img_tensor = T.ToTensor()(img_pil)
     img_fastai = Image(img_tensor)
-    x = st.write(predict(img_fastai))
+    x = st.write(predict(img_fastai,4)) 
+    # x = st.write(predict(img_fastai,4))
 
 
 hide_streamlit_style = """
@@ -62,4 +57,4 @@ hide_streamlit_style = """
             footer {visibility: hidden;}
             </style>
             """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
